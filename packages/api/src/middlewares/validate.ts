a1597c551e9359f3581a29d40598c7fa8eb2256f
LoicZHU <zhu.loic@gmail.com>
2025-07-19 23:56:39 +0200

    feat(middleware): transform data

--- packages/api/src/middlewares/validate.ts
+++ packages/api/src/middlewares/validate.ts
@@ -1,11 +1,25 @@
 import { Request, Response, NextFunction } from 'express';
 import { ZodSchema, ZodError } from 'zod';
+import { capitalizeWord } from '../shared/utils/capitalize-word.utils';
 
 export const validate =
 	(schema: ZodSchema) =>
 	(req: Request, res: Response, next: NextFunction): void => {
 		try {
-			req.body = schema.parse(req.body);
+			const parsedBody = schema.parse(req.body) as Record<string, any>;
+			const transformedBody = {
+				...parsedBody,
+			};
+
+			if (parsedBody.companyName) {
+				transformedBody.companyName = capitalizeWord(parsedBody.companyName.trim());
+			}
+			if (parsedBody.location) {
+				transformedBody.location = capitalizeWord(parsedBody.location.trim());
+			}
+
+			req.body = transformedBody;
+
 			next();
 		} catch (err) {
 			if (err instanceof ZodError) {
