130585bc7f0a6835fcaa2aca31a9ac6c8636f049
LoicZHU <zhu.loic@gmail.com>
2025-07-20 10:34:13 +0200

    feat: add helmet

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,9 +1,14 @@
 import cors from 'cors';
 import express from 'express';
+import helmet from 'helmet';
+
 import JobsRouter from './features/jobs/router.js';
 
 const app = express();
-
-app.use(cors()).use(express.json()).use('/jobs', JobsRouter);
+app
+	.use(cors())
+	.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }))
+	.use(express.json())
+	.use('/jobs', JobsRouter);
 
 export { app };
