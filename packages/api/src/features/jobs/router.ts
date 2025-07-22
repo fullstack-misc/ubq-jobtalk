0854df70431dc9e4834acd44d9dc160ebdc1da97
LoicZHU <zhu.loic@gmail.com>
2025-07-22 22:05:25 +0200

    refactor(middleware): rename `validate` file

--- packages/api/src/features/jobs/router.ts
+++ packages/api/src/features/jobs/router.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { JobSchema } from './dtos/in/job.dto';
-import { validate } from './middlewares/validate';
+import { validateMiddleware } from './middlewares/validate.middleware';
 import { add, deleteById, editById, getStatistics, list } from './controllers';
 
 const JobsRouter = Router();
 
 JobsRouter.get('/', list)
-	.post('/', validate(JobSchema), add)
-	.patch('/:id', validate(JobSchema.partial()), editById)
+	.post('/', validateMiddleware(JobSchema), add)
+	.patch('/:id', validateMiddleware(JobSchema.partial()), editById)
 	.delete('/:id', deleteById)
 	.get('/stats', getStatistics);
 
