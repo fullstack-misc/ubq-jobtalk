b6bbceafc911812b1746421d3c380b861beeb9e3
LoicZHU <zhu.loic@gmail.com>
2025-07-23 09:46:39 +0200

    config: update `.env` variable

--- packages/ui/src/app/jobtalk/services/job-api/job-api.ts
+++ packages/ui/src/app/jobtalk/services/job-api/job-api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const jobApi = axios.create({
-	baseURL: import.meta.env.PUBLIC_JOB_API_URL || process.env.PUBLIC_JOB_API_URL || 'http://localhost:3000',
+	baseURL: import.meta.env.VITE_PUBLIC_JOB_API_URL || 'http://localhost:3000',
 	headers: {
 		'Content-Type': 'application/json',
 	},
